10aaada6a984dcf8cef7452848d034ec43d95d85
Angel VI <a.v.iliev13@gmail>
2022-11-25 12:57:19 +0200

    restructure handlers into separate files

---
+++ pkg/routes/create_keyword.go
@@ -0,0 +1,62 @@
+package routes
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/AngelVI13/fiber-investigation/pkg/database"
+)
+
+func (r *Router) HandleCreateKeywordGet(c *Ctx) error {
+	data := c.FlashData()
+
+	kwType := c.Params("kw_type")
+	data["Title"] = fmt.Sprintf("Add New %s Keyword", kwType)
+
+	return c.WithUrls().Render(CreateView, data)
+}
+
+func (r *Router) HandleCreateKeywordPost(c *Ctx) error {
+	data := c.FlashData()
+
+	kwType := c.Params("kw_type")
+	data["Title"] = fmt.Sprintf("Add New %s Keyword", kwType)
+
+	nameValue := c.FormValue("name")
+	argsValue := c.FormValue("args")
+	docsValue := c.FormValue("docs")
+
+	notAllowedCharset := "|"
+
+	if strings.ContainsAny(nameValue, notAllowedCharset) ||
+		strings.ContainsAny(argsValue, notAllowedCharset) ||
+		strings.ContainsAny(docsValue, notAllowedCharset) {
+		// TODO: how to keep the filled data after the refresh
+		return c.WithError(
+			fmt.Sprintf(
+				`Can't create new Keyword '%s'!
+                Some of the fields below contains one or more not allowed characters(%s)`,
+				nameValue,
+				notAllowedCharset,
+			)).RedirectBack(IndexUrl)
+
+	}
+
+	err := database.InsertNewKeyword(
+		r.db,
+		nameValue,
+		argsValue,
+		docsValue,
+		kwType,
+	)
+	if err != nil {
+		return c.WithError(
+			fmt.Sprintf("Failed to create new Keyword '%s'!", c.FormValue("name")),
+		).RedirectBack(IndexUrl)
+	}
+
+	// add message that kw was successfully added
+	return c.WithSuccess(
+		fmt.Sprintf("Added new Keyword '%s'", c.FormValue("name")),
+	).RedirectBack(IndexUrl)
+}
---
+++ pkg/routes/keywords.go
@@ -0,0 +1,89 @@
+package routes
+
+import (
+	"fmt"
+	"html/template"
+
+	"github.com/AngelVI13/fiber-investigation/pkg/database"
+)
+
+func (r *Router) HandleBusinessKeywords(c *Ctx) error {
+	data := c.FlashData()
+	data["Title"] = "Business Keywords"
+
+	keywords, err := database.BusinessKeywords(r.db)
+	if err != nil {
+		return c.WithUrls().WithError(fmt.Sprintf(
+			"error while fetching business keywords: %v", err),
+		).Render(KeywordsView, data)
+	}
+
+	latestVersion, allVersions, err := database.LatestAndAllVersions(r.db)
+	if err != nil {
+		return c.WithError(fmt.Sprintf(
+			"Failed to get Versioning info. Error: %v", err),
+		).Redirect(IndexUrl)
+	}
+
+	data["Keywords"] = keywords
+	data["KwType"] = template.JS(database.Business)
+	data["Versions"] = allVersions
+	data["LatestVersion"] = latestVersion.ID
+	data["SelectedVersion"] = latestVersion.ID
+
+	return c.WithUrls().Render(KeywordsView, data)
+}
+
+func (r *Router) HandleTechnicalKeywords(c *Ctx) error {
+	data := c.FlashData()
+	data["Title"] = "Technical Keywords"
+
+	keywords, err := database.TechnicalKeywords(r.db)
+	if err != nil {
+		return c.WithUrls().WithError(fmt.Sprintf(
+			"error while fetching technical keywords: %v", err),
+		).Render(KeywordsView, data)
+	}
+
+	latestVersion, allVersions, err := database.LatestAndAllVersions(r.db)
+	if err != nil {
+		return c.WithError(fmt.Sprintf(
+			"Failed to get Versioning info. Error: %v", err),
+		).Redirect(IndexUrl)
+	}
+
+	data["Keywords"] = keywords
+	data["KwType"] = template.JS(database.Technical)
+	data["Versions"] = allVersions
+	data["LatestVersion"] = latestVersion.ID
+	data["SelectedVersion"] = latestVersion.ID
+
+	return c.WithUrls().Render(KeywordsView, data)
+}
+
+func (r *Router) HandleAllKeywords(c *Ctx) error {
+	data := c.FlashData()
+	data["Title"] = "All Keywords"
+
+	keywords, err := database.AllKeywords(r.db)
+	if err != nil {
+		return c.WithUrls().WithError(fmt.Sprintf(
+			"error while fetching all keywords: %v", err),
+		).Render(KeywordsView, data)
+	}
+
+	latestVersion, allVersions, err := database.LatestAndAllVersions(r.db)
+	if err != nil {
+		return c.WithError(fmt.Sprintf(
+			"Failed to get Versioning info. Error: %v", err),
+		).Redirect(IndexUrl)
+	}
+
+	data["Keywords"] = keywords
+	data["KwType"] = template.JS(database.All)
+	data["Versions"] = allVersions
+	data["LatestVersion"] = latestVersion.ID
+	data["SelectedVersion"] = latestVersion.ID
+
+	return c.WithUrls().Render(KeywordsView, data)
+}
