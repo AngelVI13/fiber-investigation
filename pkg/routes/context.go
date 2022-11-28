8a19d05460c0bcbca222e482a429248a168ba9d5
Angel VI <a.v.iliev13@gmail>
2022-11-28 10:16:28 +0200

    move flash methods to context

--- pkg/routes/context.go
+++ pkg/routes/context.go
@@ -1,8 +1,20 @@
 package routes
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/sujit-baniya/flash"
+)
 
-// Ctx Wraps a fiber Ctx in order to attach utility
+type MessageLevel string
+
+const (
+	LevelPrimary MessageLevel = "primary"
+	LevelSuccess MessageLevel = "success"
+	LevelWarning MessageLevel = "warning"
+	LevelDanger  MessageLevel = "danger"
+)
+
+// Ctx Wraps a Ctx in order to attach utility
 // functions (WithUrls, WithError, etc.)
 type Ctx struct {
 	*fiber.Ctx
@@ -16,5 +28,50 @@ func (c *Ctx) WithUrls() *Ctx {
 	}
 
 	c.Bind(data)
+	return c
+}
+
+func (c *Ctx) FlashData() fiber.Map {
+	return flash.Get(c.Ctx)
+}
+
+func (c *Ctx) WithInfo(message string) *Ctx {
+	data := fiber.Map{
+		"Message": message,
+		"Level":   LevelPrimary,
+	}
+	flash.WithInfo(c.Ctx, data)
+	return c
+}
+
+func (c *Ctx) WithSuccess(message string) *Ctx {
+	data := fiber.Map{
+		"Message": message,
+		"Level":   LevelSuccess,
+	}
+	flash.WithSuccess(c.Ctx, data)
+	return c
+}
+
+func (c *Ctx) WithError(message string) *Ctx {
+	data := fiber.Map{
+		"Message": message,
+		"Level":   LevelDanger,
+	}
+	flash.WithError(c.Ctx, data)
+	return c
+}
+
+func (c *Ctx) WithWarning(message string) *Ctx {
+	data := fiber.Map{
+		"Message": message,
+		"Level":   LevelWarning,
+	}
+	flash.WithWarn(c.Ctx, data)
+	return c
+}
+
+func (c *Ctx) WithData(data fiber.Map) *Ctx {
+	flash.WithData(c.Ctx, data)
 	return c
 }
