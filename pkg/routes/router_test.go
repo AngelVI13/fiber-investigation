916b4a79567f5c3a0d6d8c576e47fe9a1a31977c
Angel VI <a.v.iliev13@gmail>
2022-12-01 15:59:01 +0200

    wip: create keyword api test

--- pkg/routes/router_test.go
+++ pkg/routes/router_test.go
@@ -1,10 +1,12 @@
 package routes
 
 import (
+	"bytes"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
+	"github.com/AngelVI13/fiber-investigation/pkg/database"
 	"github.com/AngelVI13/fiber-investigation/pkg/dbtest"
 	"github.com/AngelVI13/fiber-investigation/pkg/session"
 	"github.com/gofiber/fiber/v2"
@@ -32,17 +34,20 @@ func TestRouter(t *testing.T) {
 
 	t.Run(ExportCsvUrl, withArgs(VerifyExportCsvGet))
 	t.Run(ExportStubsUrl, withArgs(VerifyExportStubsGet))
+	t.Run(CreateKwdUrl, withArgs(VerifyCreateKeywordPost))
 }
 
 func VerifyExportCsvGet(app *fiber.App, t *testing.T) {
 	router := NewTestRouter(t)
 	app.Get(ExportCsvUrl, Handler(router.HandleExportCsvGet))
 
-	r := httptest.NewRequest("GET", ExportCsvUrl, http.NoBody)
-	resp, _ := app.Test(r, -1)
-	t.Log(resp)
+	r := httptest.NewRequest(http.MethodGet, ExportCsvUrl, http.NoBody)
+	resp, err := app.Test(r, -1)
+	if err != nil {
+		t.Fatalf("app test request error: %v", err)
+	}
 
-	if resp.StatusCode != 201 {
+	if resp.StatusCode != 200 {
 		t.Fatalf("unexpected status code %d", resp.StatusCode)
 	}
 }
@@ -51,10 +56,59 @@ func VerifyExportStubsGet(app *fiber.App, t *testing.T) {
 	router := NewTestRouter(t)
 	app.Get(ExportStubsUrl, Handler(router.HandleExportStubsGet))
 
-	r := httptest.NewRequest("GET", ExportStubsUrl, http.NoBody)
-	resp, _ := app.Test(r, -1)
+	r := httptest.NewRequest(http.MethodGet, ExportStubsUrl, http.NoBody)
+	resp, err := app.Test(r, -1)
+	if err != nil {
+		t.Fatalf("app test request error: %v", err)
+	}
 
 	if resp.StatusCode != 200 {
 		t.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+}
+
+func VerifyCreateKeywordPost(app *fiber.App, t *testing.T) {
+	router := NewTestRouter(t)
+	app.Get(CreateKwdUrl, Handler(router.HandleCreateKeywordPost))
+
+	initialKeywords, err := database.AllKeywords(router.db)
+	if err != nil {
+		t.Fatalf("error while getting keywords: %v", err)
+	}
+
+	initialKeywordsNum := 6
+	if len(initialKeywords) != initialKeywordsNum {
+		t.Fatalf(
+			"expected %d keywords but got %d",
+			initialKeywordsNum,
+			len(initialKeywords),
+		)
+	}
+
+	// TODO: Update this
+	var jsonStr = []byte(`{"username": "aditira", "password": "1234"}`)
+	r := httptest.NewRequest(http.MethodPost, CreateKwdUrl, bytes.NewBuffer(jsonStr))
+
+	resp, err := app.Test(r, -1)
+	if err != nil {
+		t.Fatalf("app test request error: %v", err)
+	}
+
+	if resp.StatusCode != 200 {
+		t.Log(resp)
+		t.Fatalf("unexpected status code %d", resp.StatusCode)
+	}
+
+	keywords, err := database.AllKeywords(router.db)
+	if err != nil {
+		t.Fatalf("error while getting keywords: %v", err)
+	}
+
+	if len(keywords) != initialKeywordsNum+1 {
+		t.Fatalf(
+			"expected %d keywords but got %d",
+			initialKeywordsNum+1,
+			len(keywords),
+		)
 	}
 }
