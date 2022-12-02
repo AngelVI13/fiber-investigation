829bf2c583edb54aa6f473ed5a509d8f8ed7408e
Angel VI <a.v.iliev13@gmail>
2022-12-02 15:02:30 +0200

    add unittest for create kw with missing name

--- pkg/routes/router_test.go
+++ pkg/routes/router_test.go
@@ -55,6 +55,52 @@ func MultipartForm(data map[string]string) (
 	return body, writer.FormDataContentType(), nil
 }
 
+func CheckCookieStatus(resp *http.Response, status string, t *testing.T) {
+	cookies := resp.Cookies()
+	if len(cookies) < 1 {
+		t.Errorf("unexpected number of cookies: wanted 1 but got %d", len(cookies))
+	}
+
+	cookie := cookies[0]
+	if cookie.Name != FiberCookieName {
+		t.Errorf("unexpected cookied name: %s", cookie.Name)
+	}
+
+	if !strings.Contains(cookie.Value, status) {
+		t.Fatalf("expected %s to be flashed on screen but got: %s", status, cookie.Value)
+	}
+}
+
+func MakePostRequest(
+	app *fiber.App,
+	url string,
+	formData map[string]string,
+) (*http.Response, error) {
+	var req *http.Request
+
+	if len(formData) == 0 {
+		// No form data
+		req = httptest.NewRequest(http.MethodPost, url, http.NoBody)
+	} else {
+		// Build and attach form data to request
+		formBody, formType, err := MultipartForm(formData)
+		if err != nil {
+			return nil, fmt.Errorf("failed to build multipart form: %v", err)
+		}
+
+		body := bytes.NewReader(formBody.Bytes())
+		req = httptest.NewRequest(http.MethodPost, url, body)
+		req.Header.Set("Content-Type", formType)
+	}
+
+	resp, err := app.Test(req, -1)
+	if err != nil {
+		return nil, fmt.Errorf("app test request error: %v", err)
+	}
+
+	return resp, nil
+}
+
 func TestRouter(t *testing.T) {
 	app := testutil.NewTestFiberApp(t)
 	session.CreateSession()
@@ -71,6 +117,7 @@ func TestRouter(t *testing.T) {
 	t.Run(ExportCsvUrl, withArgs(VerifyExportCsvGet))
 	t.Run(ExportStubsUrl, withArgs(VerifyExportStubsGet))
 	t.Run(CreateKwdUrl, withArgs(VerifyCreateKeywordPost))
+	t.Run(CreateKwdUrl+" missing name", withArgs(VerifyCreateKeywordMissingNamePost))
 }
 
 func VerifyExportCsvGet(app *fiber.App, t *testing.T) {
@@ -122,41 +169,23 @@ func VerifyCreateKeywordPost(app *fiber.App, t *testing.T) {
 		)
 	}
 
-	body, formType, err := MultipartForm(map[string]string{
+	data := map[string]string{
 		"name": "New keyword",
 		"args": "arg1=5, arg2=10",
 		"docs": "New keyword docs.",
-	})
-	if err != nil {
-		t.Fatal(err)
 	}
 
 	url := fmt.Sprintf("%s/%s", CreateKwdUrl, database.Technical)
-	r := httptest.NewRequest(http.MethodPost, url, bytes.NewReader(body.Bytes()))
-	r.Header.Set("Content-Type", formType)
-
-	resp, err := app.Test(r, -1)
+	resp, err := MakePostRequest(app, url, data)
 	if err != nil {
-		t.Fatalf("app test request error: %v", err)
+		t.Error(err)
 	}
 
 	if resp.StatusCode != 302 {
 		t.Errorf("unexpected status code %d", resp.StatusCode)
 	}
 
-	cookies := resp.Cookies()
-	if len(cookies) < 1 {
-		t.Errorf("unexpected number of cookies: wanted 1 but got %d", len(cookies))
-	}
-
-	cookie := cookies[0]
-	if cookie.Name != FiberCookieName {
-		t.Errorf("unexpected cookied name: %s", cookie.Name)
-	}
-
-	if !strings.Contains(cookie.Value, "success") {
-		t.Fatalf("expected success to be flashed on screen but got: %s", cookie.Value)
-	}
+	CheckCookieStatus(resp, "success", t)
 
 	keywords, err := database.AllKeywords(router.db)
 	if err != nil {
@@ -184,5 +213,56 @@ func VerifyCreateKeywordPost(app *fiber.App, t *testing.T) {
 		t.Log(lstKw)
 		t.Log(lstKw.Name, lstKw.Args, lstKw.Docs)
 		t.Errorf("did not find newly created keyword: %v", keywords)
+	}
+}
+
+func VerifyCreateKeywordMissingNamePost(app *fiber.App, t *testing.T) {
+	router := NewTestRouter(t)
+	app.Post(CreateKwdUrlFull, Handler(router.HandleCreateKeywordPost))
+
+	initialKeywords, err := database.AllKeywords(router.db)
+	if err != nil {
+		t.Fatalf("error while getting keywords: %v", err)
+	}
+
+	initialKeywordsNum := 6
+	if len(initialKeywords) != initialKeywordsNum {
+		t.Fatalf(
+			"expected %d keywords but got %d",
+			initialKeywordsNum,
+			len(initialKeywords),
+		)
+	}
+
+	data := map[string]string{
+		"name": "",
+		"args": "arg1=5, arg2=10",
+		"docs": "New keyword docs.",
+	}
+
+	url := fmt.Sprintf("%s/%s", CreateKwdUrl, database.Technical)
+	resp, err := MakePostRequest(app, url, data)
+	if err != nil {
+		t.Error(err)
+	}
+
+	if resp.StatusCode != 302 {
+		t.Errorf("unexpected status code %d", resp.StatusCode)
+	}
+
+	CheckCookieStatus(resp, "error", t)
+
+	keywords, err := database.AllKeywords(router.db)
+	if err != nil {
+		t.Fatalf("error while getting keywords: %v", err)
+	}
+
+	expKeywordsNum := initialKeywordsNum
+	if len(keywords) != expKeywordsNum {
+		t.Fatalf(
+			"expected %d keywords but got %d",
+			expKeywordsNum,
+			len(keywords),
+		)
 	}
 }
